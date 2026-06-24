c471bf26a0642989025547af57d7a6ac8915ffb1
avsalavaishnavi-stack <avsala.vaishnavi@klh.edu.in>
2026-06-24 21:47:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/Travel-Mate/' : '/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
+
